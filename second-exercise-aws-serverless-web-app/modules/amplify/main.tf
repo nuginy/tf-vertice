03859715ff1ad5b53e5ecbed53fc02040a6d4f61
Miroslav Zapletal <nuginy@gmail.com>
2024-04-28 20:28:06 +0200

    Minor fixes again

--- second-exercise-aws-serverless-web-app/modules/amplify/main.tf
+++ second-exercise-aws-serverless-web-app/modules/amplify/main.tf
@@ -15,7 +15,7 @@ resource "aws_amplify_app" "web_app" {
   access_token = var.git_access_token
   enable_branch_auto_build = var.amplify_enable_branch_auto_build
   enable_auto_branch_creation = var.amplify_enable_branch_auto_creation
-  auto_branch_creation_patterns =
+  auto_branch_creation_patterns = var.amplify_branch_auto_creation_pattern
   environment_variables = {
     ENV = var.amplify_env,
     API_GW_ADDRESS = var.amplify_api_gw_address
